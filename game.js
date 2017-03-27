0efafa3ee0f02c02637c041c0b356a1cce6c8471
Viktoriia Zhovtun <vikazhovtun49@gmail.com>
2017-03-27 21:09:30 +0300

    change quantity of asteroids to 4

--- game.js
+++ game.js
@@ -941,7 +941,7 @@ Game = {
 
       Game.score = 0;
       Game.lives = 2;
-      Game.totalAsteroids = 2;
+      Game.totalAsteroids = 4;
       Game.spawnAsteroids();
 
       Game.nextBigAlienTime = Date.now() + 30000 + (30000 * Math.random());
